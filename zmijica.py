fb0bc2b68b2d42adcb59b38e04fbfc963697e15d
vaniiz-art <ivanzivaljevic88@gmail.com>
2026-06-09 15:49:20 +0200

    Kreiranje prozora za igricu

--- zmijica.py
+++ zmijica.py
@@ -1,1 +1,15 @@
+import pygame
+import random
+import sys
 
+pygame.init()
+
+sirina = 800
+visina = 600
+
+screen = pygame.display.set_mode((sirina, visina))
+pygame.display.set_caption("Snake Maze")
+
+clock = pygame.time.Clock()
+
+font = pygame.font.SysFont("Arial", 30)
