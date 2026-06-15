37925424d2c916864fc586e4b5af99690cff6aca
vaniiz-art <ivanzivaljevic88@gmail.com>
2026-06-15 12:50:59 +0200

    Sudar zmijice sa sobom

--- zmijica.py
+++ zmijica.py
@@ -6,88 +6,105 @@ pygame.init()
 
 sirina = 800
 visina = 600
-
-blok=20
+blok = 20
 
 screen = pygame.display.set_mode((sirina, visina))
 pygame.display.set_caption("Snake Maze")
 
 clock = pygame.time.Clock()
-
 font = pygame.font.SysFont("Arial", 30)
 
-crna=(15, 15, 15)
-zelena=(0, 220, 0)
-tamna_zelena=(0, 180, 0)
-crvena=(255,0,0)
+crna = (15, 15, 15)
+zelena = (0, 220, 0)
+tamna_zelena = (0, 180, 0)
+crvena = (255, 0, 0)
+
 def kreiraj_zmijicu():
     return [
         (100, 100),
         (80, 100),
         (60, 100)
     ]
+
 def kreiraj_hranu(zmijica):
-
     while True:
-
         hrana_x = random.randrange(0, sirina, blok)
         hrana_y = random.randrange(0, visina, blok)
 
-        if ((hrana_x, hrana_y) not in zmijica):
-
+        if (hrana_x, hrana_y) not in zmijica:
             return (hrana_x, hrana_y)
 
+x_osa = blok
+y_osa = 0
 
-x_osa=blok
-y_osa=0
-trenutni_level=0
-zmijica=kreiraj_zmijicu()
-hrana=kreiraj_hranu(zmijica)
-kraj_igre=False
+zmijica = kreiraj_zmijicu()
+hrana = kreiraj_hranu(zmijica)
+kraj_igre = False
 
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         if event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_UP and y_osa == 0:
-
                 x_osa = 0
                 y_osa = -blok
 
             elif event.key == pygame.K_DOWN and y_osa == 0:
-
                 x_osa = 0
                 y_osa = blok
 
             elif event.key == pygame.K_LEFT and x_osa == 0:
-
                 x_osa = -blok
                 y_osa = 0
 
             elif event.key == pygame.K_RIGHT and x_osa == 0:
-
                 x_osa = blok
                 y_osa = 0
 
-    if kraj_igre==False:
-        glava_x_osa=zmijica[0][0] + x_osa
-        glava_y_osa=zmijica[0][1] + y_osa
+    if not kraj_igre:
 
-        nova_glava=(glava_x_osa,glava_y_osa)
-        zmijica.insert(0,nova_glava)
-        if nova_glava==hrana:
-            hrana=kreiraj_hranu(zmijica)
+        glava_x_osa = zmijica[0][0] + x_osa
+        glava_y_osa = zmijica[0][1] + y_osa
+
+        nova_glava = (glava_x_osa, glava_y_osa)
+
+        if nova_glava in zmijica:
+            kraj_igre = True
         else:
-            zmijica.pop()
+            zmijica.insert(0, nova_glava)
+
+            if nova_glava == hrana:
+                hrana = kreiraj_hranu(zmijica)
+            else:
+                zmijica.pop()
 
     screen.fill(crna)
+
     for dio in zmijica:
-        pygame.draw.rect(screen,zelena,(dio[0],dio[1],blok,blok),border_radius=6)
-    pygame.draw.rect(screen,tamna_zelena,(zmijica[0][0],zmijica[0][1],blok,blok),border_radius=6)
-    pygame.draw.rect(screen,crvena,(hrana[0],hrana[1],blok,blok),border_radius=6)
+        pygame.draw.rect(screen, zelena, (dio[0], dio[1], blok, blok), border_radius=6)
+
+    pygame.draw.rect(
+        screen,
+        tamna_zelena,
+        (zmijica[0][0], zmijica[0][1], blok, blok),
+        border_radius=6
+    )
+
+    pygame.draw.rect(
+        screen,
+        crvena,
+        (hrana[0], hrana[1], blok, blok),
+        border_radius=6
+    )
+
+    if kraj_igre:
+        tekst = font.render("GAME OVER", True, (255, 0, 0))
+        screen.blit(tekst, (sirina // 2 - 100, visina // 2))
 
     pygame.display.update()
     clock.tick(10)
